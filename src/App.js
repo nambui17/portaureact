a4e391c07dfb88cc83dd5ca20cb5d02fae22468b
Nam Bui <nam.bui17@icloud.com>
2023-01-26 15:03:35 -0600

    Update about section
    Move navbar to Header section and edit About page.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import Navi from "./components/Navi";
+import React from 'react';
+import Header from './components/Header';
 import Footer from "./components/Footer";
-import React, {useState} from 'react';
 import './App.css';
-import About from './components/About';
-import Work from './components/Work';
-import Resume from './components/Resume';
-import Contact from './components/Contact';
 
 function App() {
-  const [activePage, pageSwitchHandler] = useState("about");
-  function showPage() {
-    switch (activePage) {
-      case "about":
-        return <About/>;
-      case "work":
-        return <Work/>;
-      case "resume":
-        return <Resume/>;
-      case "contact":
-        return <Contact/>;
-      default:
-        return <About/>;
-    }
-  }
   return (
       <div>
-        <Navi activePage={activePage} pageSwitchHandler={pageSwitchHandler}/>
-        <main>
-          {showPage(activePage)}
-        </main>
+        <Header/>
         <Footer />
       </div>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import {useSpring, useTransition} from '@react-spring/web';
-import '../styles/About.css';
+import React from "react";
+import { Container, Card, Row, Col, Image } from "react-bootstrap";
+import { useSpring, useTransition } from "@react-spring/web";
+import "../styles/About.css";
 
 function About() {
-    return(
-        <Container fluid className="aboutCon">
-            <h1>asdfasdf</h1>
-        </Container>
-    )
+  return (
+    <Container fluid className="aboutCon">
+      <Row className="aboutRow">
+        <Col className="d-flex justify-content-center flex-wrap profile">
+          <Image
+            roundedCircle
+            src={process.env.PUBLIC_URL + "/assets/images/me.jpg"}
+            className="myPic"
+            alt="Nam Bui"
+          />
+          <h2>
+            Hi! My name is Nam and I am a full-stack developer and chemical
+            engineer.
+          </h2>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default About;
