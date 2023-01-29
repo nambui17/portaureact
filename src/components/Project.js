2f0728a32bd802ec63e99cd0438028ba27fa9d9d
Nam Bui <nam.bui17@icloud.com>
2023-01-29 13:29:30 -0600

    Populate Work page with projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {Card } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 
-function Project() {
+function Project({title, image, info, tech, link, gh}) {
     return (
-        <Card className="col-sm-12 col-md-6">
-            <Card.Body>Something</Card.Body>
+        <Card className="col-xs-10 col-md-6 col-lg-3 m-3">
+            <Card.Img src={image}/>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>{info}</Card.Text>
+                <footer>{tech}</footer>
+                <Card.Link href={link}>Click Here!</Card.Link>
+                <Card.Link href={gh}>GitHub</Card.Link>
+            </Card.Body>
         </Card>
     )
 }
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,14 +1,56 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
-import {useSpring, useTransition} from '@react-spring/web';
-import '../styles/Work.css';
+import React from "react";
+import { Container } from "react-bootstrap";
+import { useSpring, useTransition } from "@react-spring/web";
+import Project from "./Project";
+import "../styles/Work.css";
 
 function Work() {
-    return(
-        <Container className="WorkCon">
-            <div></div>
-        </Container>
-    )
+  // list of objects of projects, project title, image url, info, and technologies used
+  const projects = [
+    {
+      id: 1,
+      title: "Bird Spotter",
+      image: "assets/images/BirdSpot.jpg",
+      info: "Uses web api's and third party api's to search for birds at a specified location",
+      tech: "node.js, leaflet, html, css",
+      link: "https://mjkonkel.github.io/bird-tracker/",
+      gh: "https://github.com/mjkonkel/bird-tracker"
+    },
+    {
+      id: 2,
+      title: "Power Trip",
+      image: "assets/images/PowerTrip.jpg",
+      info: "Uses web api's and third party api's to search for birds at a specified location",
+      tech: "node.js, leaflet, html, css",
+      link: "https://power-trip.herokuapp.com/",
+      gh: "https://github.com/nambui17/Power-Trip"
+    },
+    {
+      id: 3,
+      title: "Tlog",
+      image: "assets/images/Tlog.jpg",
+      info: "A blog website backend framework using handlebars.js, MySQL, and sequelize for tech applications",
+      tech: "Bootstrap, Handlebars.js, MySQL, Sequelize, node.js",
+      link: "https://tlog.herokuapp.com/",
+      gh: "https://github.com/nambui17/Tech-blog"
+    },
+  ];
+  // pass in as props to <Project/>
+  return (
+    <Container fluid className="d-flex flex-wrap justify-content-center">
+      {projects.map((project) => (
+        <Project
+          title={project.title}
+          image={project.image}
+          info={project.info}
+          tech={project.tech}
+          link={project.link}
+          gh={project.gh}
+          key={project.id}
+        />
+      ))}
+    </Container>
+  );
 }
 
 export default Work;
